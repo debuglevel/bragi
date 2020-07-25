2cb76b341ff68fffbb1c8c0a88206472b7a4ce60
Marc Kohaupt <debuglevel@gmail.com>
2020-07-25 22:06:05 +0200

    Remove old Greeting stuff

--- src/main/kotlin/de/debuglevel/bragi/greeting/GreetingRequest.kt
+++
@@ -1,6 +0,0 @@
-package de.debuglevel.bragi.greeting
-
-data class GreetingRequest(
-    val name: String,
-    val language: String?
-)
--- src/test/kotlin/de/debuglevel/bragi/greeting/GreetingDTO.kt
+++
@@ -1,5 +0,0 @@
-package de.debuglevel.bragi.greeting
-
-data class GreetingDTO(
-    val greeting: String
-)
