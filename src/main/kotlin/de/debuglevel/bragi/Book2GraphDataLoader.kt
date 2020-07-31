b2611fbb168b5c0b69274e92cdb7a2f1c7f42650
Marc Kohaupt <debuglevel@gmail.com>
2020-07-31 23:51:27 +0200

    Add Book2Graph importer for development

---
+++ src/main/kotlin/de/debuglevel/bragi/Book2GraphDataLoader.kt
@@ -0,0 +1,108 @@
+package de.debuglevel.bragi
+
+import de.debuglevel.book2graph.parser.xml.FodtParser
+import de.debuglevel.bragi.chapter.Chapter
+import de.debuglevel.bragi.chapter.ChapterService
+import de.debuglevel.bragi.character.Character
+import de.debuglevel.bragi.character.CharacterService
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.env.Environment
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.discovery.event.ServiceReadyEvent
+import mu.KotlinLogging
+import java.io.File
+import javax.inject.Singleton
+
+/**
+ * Populate the database with sample data imported from Book2Graph.
+ */
+@Singleton
+@Requires(notEnv = [Environment.TEST]) // Do not load data in tests.
+class Book2GraphDataLoader(
+    private val characterService: CharacterService,
+    private val chapterService: ChapterService
+) : ApplicationEventListener<ServiceReadyEvent> {
+    private val logger = KotlinLogging.logger {}
+
+    override fun onApplicationEvent(event: ServiceReadyEvent) {
+        return // remove if you've got the necessary data ;)
+        logger.debug { "Populating database with sample data..." }
+
+        val mrPreston = Character(
+            id = null,
+            name = "Mr. Preston",
+            aliases = listOf("Preston"),
+            notes = ""
+        )
+        val mrsPreston = Character(
+            id = null,
+            name = "Mrs. Preston",
+            aliases = listOf(),
+            notes = ""
+        )
+        val iraPreston = Character(
+            id = null,
+            name = "Ira Preston",
+            aliases = listOf("Ira"),
+            notes = ""
+        )
+        val charly = Character(
+            id = null,
+            name = "Charly",
+            aliases = listOf(),
+            notes = ""
+        )
+        val kayne = Character(
+            id = null,
+            name = "Kayne",
+            aliases = listOf(),
+            notes = ""
+        )
+        val sandra = Character(
+            id = null,
+            name = "Sandra",
+            aliases = listOf("San"),
+            notes = ""
+        )
+        val sirrus = Character(
+            id = null,
+            name = "Sirrus",
+            aliases = listOf(),
+            notes = ""
+        )
+        val joey = Character(
+            id = null,
+            name = "Joey",
+            aliases = listOf(),
+            notes = ""
+        )
+        val amrai = Character(
+            id = null,
+            name = "Amrai",
+            aliases = listOf(),
+            notes = ""
+        )
+
+        characterService.add(mrPreston)
+        characterService.add(mrsPreston)
+        characterService.add(iraPreston)
+        characterService.add(charly)
+        characterService.add(kayne)
+        characterService.add(sandra)
+        characterService.add(sirrus)
+        characterService.add(joey)
+        characterService.add(amrai)
+
+        val book = FodtParser.parse(File("Buch.fodt"))
+        val chapters = book.chapters.map {
+            Chapter(
+                id = null,
+                title = it.title,
+                content = it.textAsString
+            )
+        }
+        chapters.forEach { chapterService.add(it) }
+
+        logger.debug { "Populated database with sample data." }
+    }
+}
