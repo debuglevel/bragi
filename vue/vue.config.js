fee2136abfd25ae3029530d383e65378aa45a7b9
Marc Kohaupt <debuglevel@gmail.com>
2020-08-06 20:37:48 +0000

    Serve dev server on port 8081

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   // disable host check for gitpod
   devServer: {
-    disableHostCheck: true
+    disableHostCheck: true,
+    port: 8081
   }
 };
