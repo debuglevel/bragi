1f9a87e2837c7c8a5f1653d033d90247bfe1094a
Marc Kohaupt <debuglevel@gmail.com>
2020-08-07 00:53:58 +0200

    Add TODO

--- vue/src/views/Chapters.vue
+++ vue/src/views/Chapters.vue
@@ -6,12 +6,7 @@
       <v-container>
         <v-row>
           <v-col cols="12" md="4">
-            <v-text-field
-              v-model="form.title"
-              :rules="titleRules"
-              label="Chapter title"
-              required
-            ></v-text-field>
+            <v-text-field v-model="form.title" :rules="titleRules" label="Chapter title" required></v-text-field>
           </v-col>
           <v-col cols="12" md="4">
             <v-btn class="mr-4" @click="onSubmit">Add chapter</v-btn>
@@ -34,7 +29,7 @@ import axios from "axios";
 export default {
   name: "Chapters",
   components: {
-    ChapterTable,
+    ChapterTable
   },
 
   data: () => ({
@@ -42,17 +37,18 @@ export default {
       // { "id": 12, "title": "blubb" }
       { id: 1, title: "Chapter 1", summary: "Summary 1" },
       { id: 2, title: "Chapter 2", summary: "Summary 2" },
-      { id: 3, title: "Chapter 3", summary: "Summary 3" },
+      { id: 3, title: "Chapter 3", summary: "Summary 3" }
     ],
     form: {
-      title: "",
+      title: ""
     },
-    titleRules: [(v) => !!v || "Title is required"],
+    titleRules: [v => !!v || "Title is required"]
   }),
   mounted() {
+    // TODO: proper axios failure handling
     axios
       .get("http://localhost:8080/chapters/")
-      .then((response) => (this.chapters = response.data));
+      .then(response => (this.chapters = response.data));
   },
   methods: {
     onSubmit(evt) {
@@ -60,8 +56,8 @@ export default {
       //alert(JSON.stringify(this.form))
       axios
         .post("http://localhost:8080/chapters/", { title: this.form.title })
-        .then((response) => this.chapters.push(response.data));
-    },
-  },
+        .then(response => this.chapters.push(response.data));
+    }
+  }
 };
 </script>
