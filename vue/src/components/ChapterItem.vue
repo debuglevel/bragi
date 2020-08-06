a29eae608b8b8f8cd1cd0ee9688389ea2ef6e3bd
Marc Kohaupt <debuglevel@gmail.com>
2020-08-07 00:38:20 +0200

    Add TODOs

--- vue/src/components/ChapterItem.vue
+++ vue/src/components/ChapterItem.vue
@@ -1,12 +1,20 @@
 <template>
   <tr>
     <td>
-      <router-link :to="'chapters/' + chapter.id">{{
+      <router-link :to="'chapters/' + chapter.id">
+        {{
         chapter.title
-      }}</router-link>
+        }}
+      </router-link>
     </td>
     <!-- <td>{{ chapter.id }}</td> -->
     <td>
+      <!--
+      TODO: dialog should be used instead of tooltip (https://vuetifyjs.com/en/components/dialogs/) 
+      TODO: same for Character and Place
+      XXX: create an own ShowNotes component or something like that for consistent usage
+      TODO: maybe a expension panel would also be good (https://vuetifyjs.com/en/components/expansion-panels/)
+      -->
       <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
           <span v-bind="attrs" v-on="on">Hover me</span>
@@ -22,10 +30,10 @@ export default {
   name: "ChapterItem",
 
   props: {
-    chapter: {},
+    chapter: {}
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
