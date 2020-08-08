071e8b69f6a43a237928934a43968b8964685c8e
Marc Kohaupt <debuglevel@gmail.com>
2020-08-09 00:40:29 +0200

    Add picture data to sample data

--- sample-data/got/characters-to-kotlin.py
+++ sample-data/got/characters-to-kotlin.py
@@ -1,4 +1,6 @@
 import json
+import base64
+import requests
 
 def print_template(character):
     name = character["characterName"]
@@ -8,12 +10,22 @@ def print_template(character):
     except KeyError:
         alias = ""
 
+    try:
+        characterImageThumb = character["characterImageThumb"]
+        base64image = get_as_base64(characterImageThumb)
+    except KeyError:
+        base64image = ""
+    except:
+        base64image = ""
+
     variableName = "`"+name+"`"
 
-    output = f"{name};{alias}"
+    output = f"{name};{alias};{base64image}"
 
     print(output)
 
+def get_as_base64(url):
+    return base64.b64encode(requests.get(url).content).decode('ascii')
 
 characters_file = open("characters.json")
 c = json.load(characters_file)
