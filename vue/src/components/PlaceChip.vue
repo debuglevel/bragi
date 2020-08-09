54a4a530b279406ac9c416ccaf4f9c079cae5e81
Marc Kohaupt <debuglevel@gmail.com>
2020-08-09 20:57:59 +0200

    Show place picture in chip

--- vue/src/components/PlaceChip.vue
+++ vue/src/components/PlaceChip.vue
@@ -1,23 +1,34 @@
 <template>
   <v-menu bottom right transition="scale-transition" origin="top left">
     <template v-slot:activator="{ on }">
-      <v-chip class="ma-1" pill v-on="on" :color="background">{{
+      <v-chip class="ma-1" pill v-on="on" :color="background">
+        <v-avatar left v-if="hasPicture">
+          <v-img :src="place.picture" />
+        </v-avatar>
+        {{
         place.name
-      }}</v-chip>
+        }}
+      </v-chip>
     </template>
 
     <v-card width="600">
       <v-list>
         <v-list-item>
+          <v-list-item-avatar v-if="hasPicture" size="96">
+            <v-img :src="place.picture" />
+          </v-list-item-avatar>
+
           <v-list-item-content>
             <v-list-item-title>{{ place.name }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
-          <v-list-item-content class="preformatted-newlines">{{
+          <v-list-item-content class="preformatted-newlines">
+            {{
             place.notes
-          }}</v-list-item-content>
+            }}
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item :to="'/places/' + place.id">
@@ -39,16 +50,24 @@ export default {
   components: {},
 
   props: {
-    place: {},
+    place: {}
   },
   data: () => ({
     //
   }),
+  methods: {
+    containsKey(obj, key) {
+      return Object.keys(obj).includes(key);
+    }
+  },
   computed: {
     background: function() {
       return ColorService.generateRandomHexColor(this.place.id);
     },
-  },
+    hasPicture: function() {
+      return this.containsKey(this.place, "picture");
+    }
+  }
 };
 </script>
 
