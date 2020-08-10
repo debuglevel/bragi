768a1026ccf0e52f4a1657fe749872baa490c9a4
Marc Kohaupt <debuglevel@gmail.com>
2020-08-10 23:31:26 +0200

    Documentation for extractFirstName()

--- src/main/kotlin/de/debuglevel/bragi/character/CharacterUtils.kt
+++ src/main/kotlin/de/debuglevel/bragi/character/CharacterUtils.kt
@@ -1,6 +1,10 @@
 package de.debuglevel.bragi.character
 
 object CharacterUtils {
+    /**
+     * Gets the first name of a full name.
+     * Does not include middle names.
+     */
     fun extractFirstName(name: String): String {
         if (name.isBlank()) {
             throw IllegalArgumentException("name must not be blank")
