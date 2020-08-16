7ed16419b93b06282ae00b866b27ce8b7edb7efd
Marc Kohaupt <debuglevel@gmail.com>
2020-08-16 17:38:43 +0200

    Better layout for sample data

--- vue/src/views/Sampledata.vue
+++ vue/src/views/Sampledata.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="sampledata">
     <h1>Populate with sample data</h1>
-    <v-card max-width="450" class="mx-auto">
-      <v-card-title>Game of Thrones</v-card-title>
-      <v-card-text>Populate database with some data from A Wiki of Ice and Fire</v-card-text>
-      <v-card-actions>
-        <v-btn @click="populateGot">Populate</v-btn>
-      </v-card-actions>
-    </v-card>
 
-    <v-card max-width="450" class="mx-auto">
-      <v-card-title>Book2Graph</v-card-title>
-      <v-card-text>Populate database with a Book2Graph ODT file</v-card-text>
-      <v-card-actions>
-        <v-btn @click="populateBook2Graph">Populate</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card max-width="450" class="mx-auto">
+            <v-card-title>Game of Thrones</v-card-title>
+            <v-card-text>Populate database with some data from A Wiki of Ice and Fire</v-card-text>
+            <v-card-actions>
+              <v-btn @click="populateGot">Populate</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card max-width="450" class="mx-auto">
+            <v-card-title>Book2Graph</v-card-title>
+            <v-card-text>Populate database with a Book2Graph ODT file</v-card-text>
+            <v-card-actions>
+              <v-btn @click="populateBook2Graph">Populate</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
